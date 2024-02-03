135d7736628df3fe30bf25f40e2f1d95e2c809a8
Colin Hudler <chudler@uchicago.edu>
2024-02-03 07:05:38 -0600

    Initial Commit

---
+++ Bedside Hub/Bedside_HubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bedside_HubApp.swift
+//  Bedside Hub
+//
+//  Created by Colin Hudler on 2/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Bedside_HubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
